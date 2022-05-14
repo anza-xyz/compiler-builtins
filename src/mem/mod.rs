cb97a05c12c69c9e7ba3ecb10535601103fb20b7
Alessandro Decina <alessandro.d@gmail.com>
2022-05-14 20:41:31 +1000

    [SOL] implement static syscalls

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -226,14 +226,53 @@ intrinsics! {
 // is performed in the run-time system instead, by calling the
 // corresponding "C" function.
 
-#[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
-extern "C" {
-    fn sol_memcpy_(dest: *mut u8, src: *const u8, n: u64);
-    fn sol_memmove_(dest: *mut u8, src: *const u8, n: u64);
-    fn sol_memset_(s: *mut u8, c: u8, n: u64);
-    fn sol_memcmp_(s1: *const u8, s2: *const u8, n: u64, result: *mut i32);
+#[cfg(all(
+    any(target_arch = "bpf", target_arch = "sbf"),
+    not(target_feature = "static-syscalls")
+))]
+mod syscalls {
+    extern "C" {
+        pub fn sol_memcpy_(dest: *mut u8, src: *const u8, n: u64);
+        pub fn sol_memmove_(dest: *mut u8, src: *const u8, n: u64);
+        pub fn sol_memset_(s: *mut u8, c: u8, n: u64);
+        pub fn sol_memcmp_(s1: *const u8, s2: *const u8, n: u64, result: *mut i32);
+    }
 }
 
+#[cfg(all(
+    any(target_arch = "bpf", target_arch = "sbf"),
+    target_feature = "static-syscalls"
+))]
+mod syscalls {
+    pub(crate) fn sol_memcpy_(dest: *mut u8, src: *const u8, n: u64) {
+        let syscall: extern "C" fn(*mut u8, *const u8, u64) =
+            unsafe { core::mem::transmute(1904002211u64) }; // murmur32 hash of "sol_memcpy_"
+        syscall(dest, src, n)
+    }
+
+    pub(crate) fn sol_memmove_(dest: *mut u8, src: *const u8, n: u64) {
+        let syscall: extern "C" fn(*mut u8, *const u8, u64) =
+            unsafe { core::mem::transmute(1128493560u64) }; // murmur32 hash of "sol_memmove_"
+        syscall(dest, src, n)
+    }
+
+    pub(crate) fn sol_memcmp_(dest: *const u8, src: *const u8, n: u64, result: *mut i32) {
+        let syscall: extern "C" fn(*const u8, *const u8, u64, *mut i32) =
+            unsafe { core::mem::transmute(1608310321u64) }; // murmur32 hash of "sol_memcmp_"
+        syscall(dest, src, n, result)
+    }
+
+    pub(crate) fn sol_memset_(dest: *mut u8, c: u8, n: u64) {
+        let syscall: extern "C" fn(*mut u8, u8, u64) =
+            unsafe { core::mem::transmute(930151202u64) }; // murmur32 hash of "sol_memset_"
+        syscall(dest, c, n)
+    }
+}
+
+#[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
+use self::syscalls::*;
+
+#[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
 const NSTORE_THRESHOLD: usize = 15;
 
 #[cfg(any(target_arch = "bpf", target_arch = "sbf"))]
